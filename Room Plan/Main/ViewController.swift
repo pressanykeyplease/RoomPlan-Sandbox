cb26e7928ef17aca2990bd82900c3caefbe5825c
e.mukhametgareev <51046544+pressanykeyplease@users.noreply.github.com>
2022-09-23 23:54:09 +0300

    Main class configured

--- Room Plan/Main/ViewController.swift
+++ Room Plan/Main/ViewController.swift
@@ -5,15 +5,118 @@
 //  Created by Eduard on 23.09.2022.
 //
 
+import RoomPlan
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - IBOutlets
+    @IBOutlet private var exportButton: UIButton?
+    @IBOutlet private var doneButton: UIBarButtonItem?
+    @IBOutlet private var cancelButton: UIBarButtonItem?
 
+    // MARK: - View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupRoomCaptureView()
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        startSession()
+    }
 
+    override func viewWillDisappear(_ flag: Bool) {
+        super.viewWillDisappear(flag)
+        stopSession()
+    }
+
+    // MARK: - IBActions
+    @IBAction func doneScanning(_ sender: UIBarButtonItem) {
+        if isScanning { stopSession() } else { cancelScanning(sender) }
+    }
+
+    @IBAction func cancelScanning(_ sender: UIBarButtonItem) {
+        navigationController?.dismiss(animated: true)
+    }
+
+    // Export the USDZ output by specifying the `.parametric` export option.
+    // Alternatively, `.mesh` exports a nonparametric file and `.all`
+    // exports both in a single USDZ.
+    @IBAction func exportResults(_ sender: UIButton) {
+        let destinationURL = FileManager.default.temporaryDirectory.appending(path: "Room.usdz")
+        do {
+            try finalResults?.export(to: destinationURL, exportOptions: .parametric)
+            let activityVC = UIActivityViewController(activityItems: [destinationURL], applicationActivities: nil)
+            activityVC.modalPresentationStyle = .popover
+            present(activityVC, animated: true, completion: nil)
+            if let popOver = activityVC.popoverPresentationController {
+                popOver.sourceView = self.exportButton
+            }
+        } catch {
+            print("Error = \(error)")
+        }
+    }
+
+    // MARK: - Private
+    private var isScanning: Bool = false
+    private var roomCaptureView: RoomCaptureView!
+    private var roomCaptureSessionConfig: RoomCaptureSession.Configuration = RoomCaptureSession.Configuration()
+    private var finalResults: CapturedRoom?
 }
 
+// MARK: - Private methods
+private extension ViewController {
+    func setupRoomCaptureView() {
+        roomCaptureView = RoomCaptureView(frame: view.bounds)
+        roomCaptureView.captureSession.delegate = self
+        roomCaptureView.delegate = self
+        view.insertSubview(roomCaptureView, at: 0)
+    }
+
+    private func startSession() {
+        isScanning = true
+        roomCaptureView?.captureSession.run(configuration: roomCaptureSessionConfig)
+        setActiveNavBar()
+    }
+
+    private func stopSession() {
+        isScanning = false
+        roomCaptureView?.captureSession.stop()
+        setCompleteNavBar()
+    }
+
+    func setActiveNavBar() {
+        UIView.animate(withDuration: 1.0, animations: {
+            self.cancelButton?.tintColor = .white
+            self.doneButton?.tintColor = .white
+            self.exportButton?.alpha = 0.0
+        }, completion: { complete in
+            self.exportButton?.isHidden = true
+        })
+    }
+
+    func setCompleteNavBar() {
+        exportButton?.isHidden = false
+        UIView.animate(withDuration: 1.0) {
+            self.cancelButton?.tintColor = .systemBlue
+            self.doneButton?.tintColor = .systemBlue
+            self.exportButton?.alpha = 1.0
+        }
+    }
+}
+
+// MARK: - RoomCaptureViewDelegate
+extension ViewController: RoomCaptureViewDelegate {
+    func captureView(shouldPresent roomDataForProcessing: CapturedRoomData, error: Error?) -> Bool {
+        return true
+    }
+
+    // Access the final post-processed results.
+    func captureView(didPresent processedResult: CapturedRoom, error: Error?) {
+        finalResults = processedResult
+    }
+}
+
+// MARK: - RoomCaptureSessionDelegate
+extension ViewController: RoomCaptureSessionDelegate {
+}
