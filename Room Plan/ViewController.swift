35ef232d3a265541c09ca86f94c0f30ebc05dd27
e.mukhametgareev <51046544+pressanykeyplease@users.noreply.github.com>
2022-09-23 23:31:06 +0300

    Initial Commit

---
+++ Room Plan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Room Plan
+//
+//  Created by Eduard on 23.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
