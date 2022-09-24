cc05cf2522ea38740075cc9ad8c9cefc727bbfff
e.mukhametgareev <51046544+pressanykeyplease@users.noreply.github.com>
2022-09-24 23:52:10 +0300

    Nodes counting updated

--- Room Plan/PlanViewer/PlanViewerViewController.swift
+++ Room Plan/PlanViewer/PlanViewerViewController.swift
@@ -15,13 +15,24 @@ class PlanViewerViewController: UIViewController {
         guard let scene = try? SCNScene(url: url) else {
             fatalError("Can't make scene")
         }
-        let wall = scene.rootNode.childNodes { node, _ in
+        let walls = scene.rootNode.childNodes { node, _ in
             node.name!.hasPrefix("Wall") && !node.name!.hasSuffix("_grp")
-        }.first
-
-        let x = wall?.transform.m41
-        let y = wall?.transform.m42
-        let width = wall?.boundingBox.max.x
-        let angle = wall?.eulerAngles.y
+        }
+        walls.forEach {
+            let positionX = $0.transform.m41 * 10
+            let positionY = $0.transform.m43 * 10
+            let width = $0.boundingBox.max.x * 10
+            let depth = $0.boundingBox.max.y * 10
+            let angle = $0.eulerAngles.y
+            let wallView = UIView(frame: CGRect(x: Int(positionX) + 100, y: Int(positionY) + 100, width: Int(width), height: Int(depth)))
+            wallView.backgroundColor = .red
+            wallView.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
+            view.addSubview(wallView)
+            
+            
+            
+            
+            print($0.name!, positionX, positionY, width, depth, angle)
+        }
     }
 }
