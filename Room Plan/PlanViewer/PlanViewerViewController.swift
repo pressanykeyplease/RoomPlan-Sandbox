13d6951b9f73a3e460e58400822338d36ac8a5ae
e.mukhametgareev <51046544+pressanykeyplease@users.noreply.github.com>
2022-09-28 00:09:23 +0500

    Layout updated

--- Room Plan/PlanViewer/PlanViewerViewController.swift
+++ Room Plan/PlanViewer/PlanViewerViewController.swift
@@ -24,20 +24,45 @@ class PlanViewerViewController: UIViewController {
         var walls = scene.rootNode.childNodes { node, _ in
             node.name!.hasPrefix("Wall") && !node.name!.hasSuffix("_grp")
         }
+        // walls.removeLast(5)
         walls.forEach {
-            let width = ($0.boundingBox.max.x - $0.boundingBox.min.x) * 10
-            let wallView = UIView(frame: CGRect(x: 0, y: 0, width: Int(width), height: 1))
+            let width = ($0.boundingBox.max.x - $0.boundingBox.min.x) * 15
+            let wallView = UIView(frame: CGRect(x: 0, y: 0, width: Int(width), height: 3))
             wallView.backgroundColor = .black
-            let positionX = $0.worldPosition.x * 10 + 100
-            let positionY = $0.worldPosition.z * 10 + 100
+            let positionX = $0.worldPosition.x * 15 + 120
+            let positionY = $0.worldPosition.z * 15 + 120
             wallView.center = CGPoint(x: Int(positionX), y: Int(positionY))
-            wallView.transform = CGAffineTransform(rotationAngle: getRotationAngle(groupAngle: wallsGroupAngle, node: $0))
+            wallView.transform = CGAffineTransform(rotationAngle: getRotationAngle(groupAngle: wallsGroupAngle, node: $0, rootNode: scene.rootNode))
+            
+            
             view.addSubview(wallView)
         }
     }
 
-    func getRotationAngle(groupAngle: Float, node: SCNNode) -> CGFloat {
-        return CGFloat(-1 * groupAngle - node.eulerAngles.y)
+    func getRotationAngle(groupAngle: Float, node: SCNNode, rootNode: SCNNode) -> CGFloat {
+        let (min, max) = node.boundingBox
+
+        let a = SCNVector3(max.x, min.x, 0)
+        let b = SCNVector3(max.x, node.worldPosition.x, 0)
+//        let topLeft = SCNVector3(min.x, max.y, 0)
+//
+//        let worldBottomLeft = node.convertPosition(bottomLeft, to: rootNode)
+//        let worldTopRight = node.convertPosition(topRight, to: rootNode)
+//        let worldTopLeft = node.convertPosition(topLeft, to: rootNode)
+//
+//        let a = SCNVector3(x: worldBottomLeft.x - worldTopRight.x,
+//                            y: worldBottomLeft.y - worldTopRight.y,
+//                            z: worldBottomLeft.z - worldTopRight.z)
+//
+//        let b = SCNVector3(x: worldBottomLeft.x - worldTopLeft.x,
+//                            y: worldBottomLeft.y - worldTopLeft.y,
+//                            z: worldBottomLeft.z - worldTopLeft.z)
+//        print(min)
+        let vector = SCNVector3(a.y * b.z - a.z * b.y,
+                                -1 * (a.x * b.z - a.z * b.x),
+                                a.x * b.y - a.y * b.x)
+
+        return CGFloat(vector.y)
     }
 }
 
