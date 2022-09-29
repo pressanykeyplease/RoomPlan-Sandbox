1fcaefdc82b75a7f3f9824cd28fddc4f6215aa15
e.mukhametgareev <51046544+pressanykeyplease@users.noreply.github.com>
2022-09-29 10:01:11 +0500

    Angle correction added

--- Room Plan/PlanViewer/PlanViewerViewController.swift
+++ Room Plan/PlanViewer/PlanViewerViewController.swift
@@ -24,7 +24,6 @@ class PlanViewerViewController: UIViewController {
         var walls = scene.rootNode.childNodes { node, _ in
             node.name!.hasPrefix("Wall") && !node.name!.hasSuffix("_grp")
         }
-        // walls.removeLast(5)
         walls.forEach {
             let width = ($0.boundingBox.max.x - $0.boundingBox.min.x) * 15
             let wallView = UIView(frame: CGRect(x: 0, y: 0, width: Int(width), height: 3))
@@ -32,17 +31,18 @@ class PlanViewerViewController: UIViewController {
             let positionX = $0.worldPosition.x * 15 + 120
             let positionY = $0.worldPosition.z * 15 + 120
             wallView.center = CGPoint(x: Int(positionX), y: Int(positionY))
-            wallView.transform = CGAffineTransform(rotationAngle: getRotationAngle(groupAngle: wallsGroupAngles, node: $0, rootNode: scene.rootNode))
+            wallView.transform = CGAffineTransform(rotationAngle: getRotationAngle(groupAngle: wallsGroupAngles, nodeAngles: $0.eulerAngles, rootNode: scene.rootNode))
             
             
             view.addSubview(wallView)
         }
     }
 
-    func getRotationAngle(groupAngle: SCNVector3, node: SCNNode, rootNode: SCNNode) -> CGFloat {
-        let nodeAngle = node.eulerAngles.y
-        print(node.eulerAngles.x, node.eulerAngles.z)
-        return CGFloat(-1 * groupAngle.y - nodeAngle)
+    func getRotationAngle(groupAngle: SCNVector3, nodeAngles: SCNVector3, rootNode: SCNNode) -> CGFloat {
+        var extraAngle: Float {
+            nodeAngles.z == 0 ? 0 : .pi / 2
+        }
+        return CGFloat(-1 * groupAngle.y - nodeAngles.y + extraAngle)
     }
 }
 
