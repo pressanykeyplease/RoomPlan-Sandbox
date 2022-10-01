0a70085a041fe643a13983307fd32a016d5ee3e1
e.mukhametgareev <51046544+pressanykeyplease@users.noreply.github.com>
2022-10-01 17:09:59 +0500

    Transform added

--- Room Plan/PlanViewer/PlanViewerViewController.swift
+++ Room Plan/PlanViewer/PlanViewerViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 class PlanViewerViewController: UIViewController {
     // MARK: - Public
     func configure(with url: URL) {
-        // let url = Bundle.main.url(forResource: "room_3", withExtension: "usdz")!//  - example
+        // let url = Bundle.main.url(forResource: "room_4", withExtension: "usdz")!//  - example
         guard let scene = try? SCNScene(url: url) else {
             fatalError("Can't make scene")
         }
@@ -24,27 +24,36 @@ class PlanViewerViewController: UIViewController {
         let walls = scene.rootNode.childNodes { node, _ in
             node.name!.hasPrefix("Wall") && !node.name!.hasSuffix("_grp")
         }
+        let container = UIView(frame: CGRect(x: edgeSize, y: edgeSize, width: containerSize, height: containerSize))
         walls.forEach {
-            placeWall(with: $0, wallsGroup: wallsGroup)
+            placeWall(with: $0, wallsGroup: wallsGroup, on: container)
         }
+        view.addSubview(container)
+        container.center = view.center
+        container.transform = CGAffineTransform(rotationAngle: CGFloat(walls.first!.eulerAngles.y - wallsGroup.eulerAngles.y))
     }
 
     // MARK: - Private constants
-    private let scaleFactor: Float = 30
-    private let viewOffset: Float = 200
-    private let wallDepth = 12
+    private lazy var scaleFactor: Float = 20
+    private lazy var viewOffset: Float = 0
+    private lazy var wallDepth = 6
+    private let edgePart: CGFloat = 0.2
+    
+    private lazy var containerSize = view.bounds.width * edgePart
+    private lazy var edgeSize = view.bounds.width * (1 - edgePart) / 2
 }
 
 // MARK: - Private methods
 private extension PlanViewerViewController {
-    func placeWall(with node: SCNNode, wallsGroup: SCNNode) {
+    func placeWall(with node: SCNNode, wallsGroup: SCNNode, on view: UIView) {
         let width = (node.boundingBox.max.x - node.boundingBox.min.x) * scaleFactor
         let wallView = UIView(frame: CGRect(x: .zero, y: .zero, width: Int(width), height: wallDepth))
         wallView.backgroundColor = .black
         let positionX = node.worldPosition.x * scaleFactor + viewOffset
         let positionY = node.worldPosition.z * scaleFactor + viewOffset
         wallView.center = CGPoint(x: Int(positionX), y: Int(positionY))
-        wallView.transform = CGAffineTransform(rotationAngle: getRotationAngle(groupAngle: wallsGroup.eulerAngles, nodeAngles: node.eulerAngles))
+        let angle = getRotationAngle(groupAngle: wallsGroup.eulerAngles, nodeAngles: node.eulerAngles)
+        wallView.transform = CGAffineTransform(rotationAngle: angle)
         view.addSubview(wallView)
     }
 
